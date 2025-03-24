7523389d3f290b6e31439ccefe6c4118bbbb4afd
jawanredyah <dev2006me@gmail.com>
2025-03-24 13:33:03 +0300

    Integrate tubelight navbar as an optional component in App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -4,12 +4,19 @@ import Navbar from './components/Navbar';
 import FeaturedSection from './components/FeaturedSection';
 import EventList from './components/EventList';
 import EventDetails from './components/EventDetails';
+// Import the NavBarDemo component
+import { NavBarDemo } from './components/ui/navbar-demo';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-black">
+        {/* Original Navbar - comment this out if you want to use only the tubelight navbar */}
         <Navbar />
+        
+        {/* New Tubelight Navbar - uncomment to use */}
+        {/* <NavBarDemo /> */}
+        
         <Routes>
           <Route path="/" element={
             <>
